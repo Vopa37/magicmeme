ca70197e25fc6d31874a7d34c9889e3e5b78141d
Vojtěch  Pavelka <vojta.pavelka@gmail.com>
2021-04-06 12:34:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import image from "./dunno.jpg"
+function App() {
+  return (
+    <div className="App">
+      <img src={image} style={{width:"100%",height:"100%"}}/>
+    </div>
+  );
+}
+
+export default App;
